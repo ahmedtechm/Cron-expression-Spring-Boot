9b7f8d1d7d8ffb5c8612e964944dcead49abe81e
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-07 20:37:40 +0400

    3) Added CRON Expression that runs a job every day at 2:30 PM

--- src/main/java/com/codeline/CronexpressionSpringBoot/Schedule.java
+++ src/main/java/com/codeline/CronexpressionSpringBoot/Schedule.java
@@ -23,4 +23,10 @@ public class Schedule {
     public void scheduleTask_2() {
         System.out.println("Executed task Every hour");
     }
+
+    //Problem 3: Write a CRON expression that runs a job every day at 2:30 PM.
+    @Scheduled(cron = " 0 30 14 * * * ")
+    public void scheduleTask_3() {
+        System.out.println("Executed task At 02:30 PM");
+    }
 }
