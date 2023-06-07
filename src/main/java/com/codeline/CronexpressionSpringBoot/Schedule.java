e6a793786a564842cab38d1a9fe596644420008e
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-07 18:14:28 +0400

    Added Cron Expression for testing the execution

--- src/main/java/com/codeline/CronexpressionSpringBoot/Schedule.java
+++ src/main/java/com/codeline/CronexpressionSpringBoot/Schedule.java
@@ -1,8 +1,12 @@
 package com.codeline.CronexpressionSpringBoot;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-
+    @Scheduled(cron = "* * * * * *")
+    public void scheduleMessage() {
+        System.out.println("Welcome to Spring Boot Project");
+    }
 }
