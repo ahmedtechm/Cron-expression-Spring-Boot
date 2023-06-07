2fca99d826e2158669dbb3f7ff5c265c9e8d66b0
Ahmed ALAbri <ahmedtechm2050@gmail.com>
2023-06-07 18:04:05 +0400

    Added @EnableScheduling annotation to the Project

--- src/main/java/com/codeline/CronexpressionSpringBoot/CronExpressionSpringBootApplication.java
+++ src/main/java/com/codeline/CronexpressionSpringBoot/CronExpressionSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.codeline.CronexpressionSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CronExpressionSpringBootApplication {
 
 	public static void main(String[] args) {
